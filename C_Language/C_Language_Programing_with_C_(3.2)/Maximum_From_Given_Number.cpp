5b1c498d01b88fcd356301add86da05e385d21d8
Tops_Assignments <darshankalathiya963@gmail.com>
2022-09-12 11:06:37 +0530

    Done

--- C_Language/C_Language_Programing_with_C_(3.2)/Maximum_From_Given_Number.cpp
+++ C_Language/C_Language_Programing_with_C_(3.2)/Maximum_From_Given_Number.cpp
@@ -1,29 +1,23 @@
 #include<stdio.h>
 main()
 {
-	int a,b,n,remainder, maximum=0;
+	int a,n,remainder, maximum=0;
 	
 	printf("Enter Number To Find Out The Maximum From Given Number : ");
 	scanf("%d",&n);
 	
-	for(a=1; a<=n; a++)
-	{
-		while(n>0)
+	while(n!=0)
 		{
 			remainder = n%10;
-			maximum = remainder;
+			
+			if(maximum < remainder)
+			{
+				maximum = remainder;
+			}
 			n=n/10;
 		}
-	}
-		
-	for(a=1; a<=n; a++)
-	{
-		if(maximum < remainder)
-		{
-			maximum = remainder;
-		}
-	} 
-	
-	printf("\n The Maximum Value From Given Number Is = %d",maximum);
+	printf("\n\n The Maximum Value From Given Number Is = %d",maximum);	
 }
+	
+
   
